9035eb18e10920317d44f529b74b0a7d59d111cc
QiJi- <phamphat2262@gmail.com>
2024-12-01 21:12:39 +0700

    Initial commit

---
+++ src/main/java/com/webcuoiky/softwareshop/SoftwareshopApplication.java
@@ -0,0 +1,13 @@
+package com.webcuoiky.softwareshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareshopApplication.class, args);
+	}
+
+}
