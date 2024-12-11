727a12497dec51b16c0f5bfacc43056ba7c30a01
phucphan01866 <phucphan01866@gmail.com>
2024-12-11 14:47:28 +0700

    update Admin

--- src/main/java/com/webcuoiky/softwareshop/controller/webAdminController.java
+++ src/main/java/com/webcuoiky/softwareshop/controller/webAdminController.java
@@ -4,20 +4,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/admin/")
+@RequestMapping("admin")
 public class webAdminController {
-    @RequestMapping("category/product")
+
+    @RequestMapping("")
+    public  String admin() {
+
+        return "admin";
+    }
+    @RequestMapping("/product-list/product")
     public  String product() {
 
         return "admin_product";
     }
-    @RequestMapping("category")
+    @RequestMapping("/category")
     public  String category_list() {
 
         return "admin_category";
     }
-    @RequestMapping("product-list")
+    @RequestMapping("/product-list")
     public  String product_list() {
-        return "admin_list";
+
+        return "admin_product_list";
     }
 }
